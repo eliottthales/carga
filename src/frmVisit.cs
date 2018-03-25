8b5da384f5711a05b75e68c37639334e244bd67b
eliottthales <35814806+eliottthales@users.noreply.github.com>
2018-03-25 22:31:32 +0100

    Create frmVisit.cs

---
+++ src/frmVisit.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Carga
+{
+    public partial class frmVisit : Form
+    {
+        public frmVisit()
+        {
+            InitializeComponent();
+        }
+
+        private void tbl_visitBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.Validate();
+                this.tbl_visitBindingSource.EndEdit();
+                
+                MessageBox.Show("Registro salvo com sucesso!", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Falha ao salvar registro!", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void frmVisit_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'pORTARIADataSet.tbl_visit' table. You can move, or remove it, as needed.
+            
+
+        }
+
+        private void bindingNavigatorAddNewItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.tbl_visitBindingSource.EndEdit();
+            
+        }
+
+        private void bindingNavigatorDeleteItem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Deseja Excluir o Registro?", "Atenção!", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                this.Validate();
+                this.tbl_visitBindingSource.EndEdit();
+                
+
+            }
+            else
+            {
+                
+            }
+        }
+    }
+}
