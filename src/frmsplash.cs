54daa9a3d7c77d9af3d060c38327a6bc92a65409
eliottthales <35814806+eliottthales@users.noreply.github.com>
2018-03-25 22:05:14 +0100

    Create frmsplash.cs
    Loading form.

---
+++ src/frmsplash.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Carga
+{
+    public partial class frmsplash : Form
+    {
+        public frmsplash()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (progressBar1.Value < 100)
+            {
+                progressBar1.Value = progressBar1.Value + 2;
+            }
+            else
+            {
+                timer1.Enabled = false;
+                timer1.Stop();
+                this.Close();
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
